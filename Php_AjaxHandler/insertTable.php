31219c6e04fcede47fc9ff514367aa2c10f88424
account931 <account931@ukr.net>
2017-12-08 21:34:52 +0200

    Add files via upload

--- Php_AjaxHandler/insertTable.php
+++ Php_AjaxHandler/insertTable.php
@@ -1,19 +1,29 @@
 <?php
 //just a copy of selectTAble, as it contain inserting,
 
-$tableX=$_POST['serverTableID'];
-$dateX=$_POST['serverDateID'];
+$tableX=$_POST['ServerTableID'];
 $unixX=$_POST['ServerDate_UnixID'];// Unix  var
-                     
-echo "We  got-> </br>";
-echo $tableX."</br>";
-echo $dateX."</br>";
-echo $unixX."</br>";
+$interval=$_POST['serverInterval'];// Time interval   ServerName
+$nameZ=$_POST['ServerName'];// Time interval     
+$TimeNormal=$_POST['TimeNormal']; // i.e 2017-11-29
 
+                 
+echo "Insert handler status-> </br>";
+echo $tableX."</br>";
+echo $unixX."</br>";
+echo $interval."</br>";
+echo $nameZ."</br>";
 
 
 
 //$resFR = $db->query("SELECT * FROM bookingTable WHERE mvcFr_who LIKE '{$_SESSION['login']}' ");
+
+
+
+
+
+
+
 
 
 
@@ -27,65 +37,126 @@ echo $unixX."</br>";
 
 
 include '../Classes/ConnectDB.php';
+include '../Classes/DB_INSERT.php';// not working Class
 $singeltone=ConnectDB::getInstance(); //creates connection $con;  //was deactivated in index.php
 
 
 
 
+// Check if any record exist, prevent dublicate inserting
+//-------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Starts INSERTING -will be used later-------------------
+ 
 try {
-    // $conn = new PDO("mysql:host=$servername;dbname=Booking_My", $username, $password); // commented as it is in $singeltone=ConnectDB::getInstance();
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  
+    
+$resFR = $conn->query("SELECT * FROM bookingTable WHERE b_bookedUnix ='{$_POST['ServerDate_UnixID']}' AND 	b_table ='{$_POST['ServerTableID']}' AND 	b_timeInterval ='{$_POST['serverInterval']}'ORDER BY b_timeInterval ASC     "); 
+if($resFR->rowCount()==0) {
+    
+      echo "<p style='background-color:green;'>Feel free to take</p>"; 
+       //DB_INSERT:: Booking_Date(); //Insert algorithm located in  Classes/DB_INSERT.php
+      //Booking_Date();
+
+
+
+
+//Start INSERT (from function StartBooking())---------
+$sth = $conn ->prepare("INSERT INTO bookingTable(b_booker, b_ip, b_table, b_timeInterval,b_bookedDate, b_bookedUnix) VALUES (:logins, :ip, :tableID, :timeInterval,  :bookedDate,  :bookedDateUnix) ");
+
+          $sth->bindValue(':logins',  $_POST['ServerName']);   // Who is the booker
+          $sth->bindValue(':ip',      $_SERVER['REMOTE_ADDR']);//IP
+          $sth->bindValue(':tableID', $_POST['ServerTableID']);// TAble Number ID
+          $sth->bindValue(':timeInterval',  $_POST['serverInterval']);
+          $sth->bindValue(':bookedDate',    $_POST['TimeNormal']);  //$_POST['serverDateID']  //2017-11-29
+          $sth->bindValue(':bookedDateUnix',$_POST['ServerDate_UnixID'] );
+          //$sth->bindValue(':whenBooked',date("Y-m-d H:i:s") );
+    
+          $sth->execute();
+
+//END INSERT (from function StartBooking())---------
+
 
 
 //-----------------
-$sth = $conn ->prepare("INSERT INTO bookingTable(b_booker, b_ip, b_table, b_timeInterval,b_bookedDate, b_bookedUnix,  b_when_was_booked) VALUES (:logins, :ip, :passs, :timeInterval,  :bookedDate,  :bookedDateUnix, :whenBooked)");
+                          } 
+else {echo "</br>SomeOne has taken it while u were thinking";}  
 
-          $sth->bindValue(':logins',"Dima");
-          $sth->bindValue(':passs',1);
-          $sth->bindValue(':ip' ,$_SERVER['REMOTE_ADDR']);
-          $sth->bindValue(':timeInterval' ,9);
-          $sth->bindValue(':bookedDate', '2017-11-29');  //$_POST['serverDateID'] 
-          $sth->bindValue(':bookedDateUnix' ,$_POST['ServerDate_UnixID'] );
-          $sth->bindValue(':whenBooked' ,date("Y-m-d H:i:s") );
+    } //end try
+
+
+
+catch(PDOException $e) {
+    echo "ERR-ed";
+    echo "Error: " . $e->getMessage();
+                        }
+$conn = null;
+
+
+
+
+
+// END Check if any record exist, prevent dublicate inserting
+//-------------
+
+
+
+
+
+
+
+
+// Start StartBooking()
+// **************************************************************************************
+// **************************************************************************************
+//                                                                                     **  
+function Booking_Date (){
+
+try {
+ 
+//b_when_was_booked  :whenBooked
+//-----------------
+$sth = $conn ->prepare("INSERT INTO bookingTable(b_booker, b_ip, b_table, b_timeInterval,b_bookedDate, b_bookedUnix) VALUES (:logins, :ip, :tableID, :timeInterval,  :bookedDate,  :bookedDateUnix) ");
+
+          $sth->bindValue(':logins',$_POST['ServerName']);   // Who is the booker
+          $sth->bindValue(':ip',$_SERVER['REMOTE_ADDR']);//IP
+          $sth->bindValue(':tableID',$_POST['ServerTableID']);// TAble Number ID
+          $sth->bindValue(':timeInterval',$_POST['serverInterval']);
+          $sth->bindValue(':bookedDate', 'null');  //$_POST['serverDateID']  //2017-11-29
+          $sth->bindValue(':bookedDateUnix',$_POST['ServerDate_UnixID'] );
+          //$sth->bindValue(':whenBooked',date("Y-m-d H:i:s") );
     
           $sth->execute();
 //-----------------
-   /* $sql = "INSERT INTO bookingTable (b_booker,b_table,b_timeInterval)
-    VALUES ('Dima', 1, 9)";
-    // use exec() because no results are returned
-    $conn->exec($sql);*/
+   
 
 
-    echo "New record created successfully";
-    }
+    echo "New booking record created successfully";
+    } //end try
 catch(PDOException $e)
     {
-    echo "screwed</br>";
-    echo $sql . "<br>" . $e->getMessage();
+    echo "Failed booking</br>";
+    echo $e->getMessage();
     }
 
 $conn = null;
 
-//END   INSERTING -will be used later-------------------
+ }
 
 
-//-------------------------------
+// **                                                                                  **
+// **************************************************************************************
+// **************************************************************************************
+// END StartBooking()
+
+
+
+
+
+
+
+
+
+
 
 
 
--- Php_AjaxHandler/selectTable.php
+++ Php_AjaxHandler/selectTable.php
@@ -3,7 +3,7 @@
 // creates shedule,creates a hfer "book it"
 
 $tableX=$_POST['serverTableID']; //Table number
-$dateX=$_POST['serverDateID']; // Date timestamp // not used
+$dateX=$_POST['serverDateID']; // Date timestamp // 
 $unixX=$_POST['ServerDate_UnixID'];// Unix  var
                      
 echo "We  got-> </br>";
@@ -104,6 +104,8 @@ $bIntervals=array();// array for intervals available
                              }
 
 
+ echo "<div>"; //erase
+
 		for($i=9; $i<18; $i++){
              //if time exists in array  $bIntervals, displayas taken
              if(in_array($i, $bIntervals)){ $t=$i+1; // next hour
@@ -117,13 +119,29 @@ $bIntervals=array();// array for intervals available
                    $tz=$i+1; // i.e (10.00-Stz.00)=(10.00-11.00)
                    $unix=$_POST['ServerDate_UnixID'];//$rowF[0]['b_bookedUnix'];// first Unix stamp from row-any of them fits, Unixtime to book
                    $table=$_POST['serverTableID'];//$rowF[0]['b_table']; //table to book
+                   $timeNormal=$_POST['serverDateID'];// normal time like 2017-11-29
                    
-                   //echo "<div>";
-                   echo "<h6 class='free accordition'> Free =>  ".$i.  ".00-" .$tz. ".00       <span class='bookLink'  id='tbTime-$i&d-$unix&tableId-$table' > book it</span> ";
+                   //echo "<div class='accordition'>";
+//---------------------------------
+                   echo "<h6 class='free accordition bookLink2'> Free =>  ".$i.  ".00-" .$tz. ".00        <span class='bookLink'  id='' > book it</span>   </h6>";
+
+                   echo "<p style='display:none;margin-top:0.7em;background-color:;' class='nnn'>  Your name <input class='nameX' type='text'size='7' placeholder='name...'/> <button type='button' class='bookFinal' id='tbTime-$i&d-$unix&tableId-$table&timeNormal-$timeNormal' > OK </button>  </p>";
+
+                   //echo "</h6>"; // issue was here , JQ .next() is used for sibling inside PARENT// echo "<p class='free accordition'>
+
+//-------------------------------------------
+
+
+
+
+//PAtch----
+                  /*echo "<h6 class='free accordition'> Free =>  ".$i.  ".00-" .$tz. ".00       <span class='bookLink'  id='tbTime-$i&d-$unix&tableId-$table' > book it</span> ";
 
                    echo "<p style='display:none;margin-top:0.7em;background-color:;' class='nnn'>  Your name <input class='nameX' type='text'size='7' placeholder='name...'/> <button type='button' class='bookFinal' id='tbTime-$i&d-$unix&tableId-$table' > OK </button>  </p>";
 
                    echo "</h6>"; // issue was here , JQ .next() is used for sibling inside PARENT// echo "<p class='free accordition'>
+*/
+//END PAtch---
 
 
                    //echo "</div>";
@@ -132,6 +150,10 @@ $bIntervals=array();// array for intervals available
 
 		}  // END for($i=9; $i<19; $i++){
 
+
+
+
+ echo "</div>"; //erase
 
 // END  alternative---------------------
 
